3f5658edecd6c9fec96e3e7ee47d29ee3b70d027
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-27 17:50:43 -0400

    Remove App Packages
    Removes the default UWP apps

---
+++ UWP_Apps/RemoveAppPackages.ps1
@@ -0,0 +1,97 @@
+
+# https://github.com/W4RH4WK/Debloat-Windows-10/blob/47c2666ddd162a511a6517d74502d3dc8465a430/scripts/remove-default-apps.ps1#L64-L98
+
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
+
+echo "Elevating priviledges for this process"
+do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+
+echo "Uninstalling default apps"
+$apps = @(
+    # default Windows 10 apps
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.Office.OneNote"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    #"Microsoft.Windows.Photos"
+    "Microsoft.WindowsAlarms"
+    #"Microsoft.WindowsCalculator"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.WindowsSoundRecorder"
+    #"Microsoft.WindowsStore"
+    "Microsoft.XboxApp"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.MinecraftUWP"
+
+    # non-Microsoft
+    "9E2F88E3.Twitter"
+    "Flipboard.Flipboard"
+    "ShazamEntertainmentLtd.Shazam"
+    "king.com.CandyCrushSaga"
+    "ClearChannelRadioDigital.iHeartRadio"
+
+    # apps which cannot be removed using Remove-AppxPackage
+    #"Microsoft.BioEnrollment"
+    #"Microsoft.MicrosoftEdge"
+    #"Microsoft.Windows.Cortana"
+    #"Microsoft.WindowsFeedback"
+    #"Microsoft.XboxGameCallableUI"
+    #"Microsoft.XboxIdentityProvider"
+    #"Windows.ContactSupport"
+)
+
+foreach ($app in $apps) {
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+
+    Get-AppXProvisionedPackage -Online |
+        where DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online
+}
+
+echo "Force removing system apps"
+$needles = @(
+    # "Anytime"
+    # "BioEnrollment"
+    # "Browser"
+    # "ContactSupport"
+    # "Cortana"       # This will disable startmenu search.
+    # "Defender"
+    # "Feedback"
+    # "Flash"
+    # "Gaming"
+    # "InternetExplorer"
+    # "Maps"
+    # "OneDrive"
+    # "Wallet"
+    # "Xbox"
+)
+
+foreach ($needle in $needles) {
+    $pkgs = (ls "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\Packages" |
+        where Name -Like "*$needle*")
+
+    foreach ($pkg in $pkgs) {
+        $pkgname = $pkg.Name.split('\')[-1]
+
+        Takeown-Registry($pkg.Name)
+        Takeown-Registry($pkg.Name + "\Owners")
+
+        Set-ItemProperty -Path ("HKLM:" + $pkg.Name.Substring(18)) -Name Visibility -Value 1
+        New-ItemProperty -Path ("HKLM:" + $pkg.Name.Substring(18)) -Name DefVis -PropertyType DWord -Value 2
+        Remove-Item      -Path ("HKLM:" + $pkg.Name.Substring(18) + "\Owners")
+
+        dism.exe /Online /Remove-Package /PackageName:$pkgname /NoRestart
+    }
+}
