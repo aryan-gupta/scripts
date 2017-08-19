801da74fc0ad828d828048c06d42c4bc78e1249b
Aryan Gupta <kuantum.freak@gmail.com>
2017-08-19 13:41:15 -0400

    Fix Manage Optional Features script

---
+++ PSScripts/ManageOptionalFeatures.ps1
@@ -0,0 +1,23 @@
+
+$featuresOff = @(
+	"MediaPlayback"
+	"WorkFolders-Client"
+	"Microsoft-Windows-HyperV-Guest-Package"
+	"Xps-Foundation-Xps-Viewer"
+	"Printing-XPSServices-Features"
+	"SMB1Protocol"
+	"FaxServicesClientPackage"
+)
+
+foreach ($feature in $featuresOff) {
+	Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName $feature | Out-Null
+	(Get-WindowsOptionalFeature -Online -FeatureName $feature | Select-Object -Property FeatureName,State | Format-Table -HideTableHeaders | Out-String).Trim()
+}
+
+$featuresOn = @(
+	"NetFx3"
+)
+
+foreach ($feature in $featuresOn) {
+	Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName $feature
+}
