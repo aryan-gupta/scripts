2302faed10ecccfdd5716054f1612a27b8389797
Aryan Gupta <kuantum.freak@gmail.com>
2017-09-01 19:55:46 -0400

    Add Console Colors to PoSh Profile

--- PSScripts/Microsoft.PowerShell_profile.ps1
+++ PSScripts/Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,18 @@
 New-Alias npp "C:\Program Files\Notepad++\notepad++.exe"
 
 # Change working directory to C:\WORKING\
-function Set-LocationToWorking { Set-Location "C:\WORKING\" }
-New-Alias work Set-LocationToWorking
+New-Alias op Open-Project
+
+New-Alias nf New-Folder
+
+# Set console colors 
+$host.UI.RawUI.ForegroundColor = "white"
+$host.UI.RawUI.BackgroundColor = "black"
+$host.PrivateData.VerboseForegroundColor = "white"
+$host.PrivateData.VerboseBackgroundColor = "black"
+$host.PrivateData.WarningForegroundColor = "yellow"
+$host.PrivateData.WarningBackgroundColor = "black"
+$host.PrivateData.ErrorForegroundColor = "red"
+$host.PrivateData.ErrorBackgroundColor = "black"
+# Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White
+Clear-Host
