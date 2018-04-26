734aefebf996b021fc2d772513f735acafa11905
Aryan Gupta <kuantum.freak@gmail.com>
2018-04-26 18:50:14 -0400

    Add new profile script

--- PSScripts/Microsoft.PowerShell_profile.ps1
+++ PSScripts/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,17 @@
-# https://stackoverflow.com/questions/24914589/how-to-create-permanent-powershell-aliases
+Import-Module posh-git
 
-# Set alias so edit opens npp
-New-Alias npp "C:\Program Files\Notepad++\notepad++.exe"
+function Global:prompt {
+	$location = $PWD.tostring()
+	$location = $location.
+		replace("C:\Users\Aryan", "@").
+		replace("@\Projects", "#")
+	Write-VcsStatus
+	Write-Host $location -ForegroundColor Blue
+	Write-Host "PS>" -NoNewLine
+	return " "
+}
 
-# Change working directory to C:\WORKING\
-function Set-LocationToWorking { Set-Location "C:\WORKING\" }
-New-Alias work Set-LocationToWorking
+New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
+[console]::ForegroundColor = "White"
+[console]::BackgroundColor = "Black"
+Clear-Host
