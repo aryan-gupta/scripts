48b7d1e76e5723d4eeabf5e696f2d556d4b0f30e
Aryan Gupta <kuantum.freak@gmail.com>
2018-04-26 21:32:46 -0400

    Modify many files for improvement

--- Install-All.ps1
+++ Install-All.ps1
@@ -1,12 +1,17 @@
 
 # Copy PS Profile to the correct location
-Copy-Item -Path ".\PSScripts\Microsoft.PowerShell_profile.ps1" -Destination "$profile"
+# Copy-Item -Path ".\PSScripts\Microsoft.PowerShell_profile.ps1" -Destination "$profile"
 
-& ".\PSScripts\Add-RegistryDrive.ps1"
-& ".\PSScripts\Optimize-AppPackages.ps1"
-& ".\PSScripts\Optimize-OptionalFeatures.ps1"
-& ".\PSScripts\Optimize-ScheduledTask.ps1"
-& ".\PSScripts\Optimize-Services.ps1"
-& ".\PSScripts\Optimize-Taskbar.ps1"
-& ".\PSScripts\Restore-CommonDirectories.ps1"
+# & ".\PSScripts\Add-RegistryDrive.ps1"
+# & ".\PSScripts\Optimize-AppPackages.ps1"
+# & ".\PSScripts\Optimize-OptionalFeatures.ps1"
+# & ".\PSScripts\Optimize-ScheduledTask.ps1"
+# & ".\PSScripts\Optimize-Services.ps1"
+# & ".\PSScripts\Optimize-Taskbar.ps1"
+# & ".\PSScripts\Restore-CommonDirectories.ps1"
 
+# foreach ($file in $(Get-ChildItem .\RegOpti\*.reg)) {
+	# regedit /s $file.name
+# }
+
+cmd.exe /C ".\GodMode\GodMode.bat"
