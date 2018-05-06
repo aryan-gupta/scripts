ed673f30bd95df745795dfc77f38650252a39a0c
Aryan Gupta <kuantum.freak@gmail.com>
2018-05-06 13:03:15 -0400

    Improve Backup script

--- PSScripts/Backup-Home.ps1
+++ PSScripts/Backup-Home.ps1
@@ -1,28 +1,25 @@
 
-$BACKUP_SERVER = '192.168.0.5'
+$BACKUP_SERVER = 'graviton.gempi.re'
 $BACKUP_SHARE  = 'Backups'
 
+$SOURCE_DIR = @{
+	'HOME' = 'C:\Users\Aryan'
+}
+
 $SKIP_DIR = @(
-	'Downloads'
-	'AppData\Local'
-	'AppData\LocalLow'
+	$SOURCE_DIR['HOME'] + '\Downloads'
+	$SOURCE_DIR['HOME'] + '\AppData\Local'
+	$SOURCE_DIR['HOME'] + '\AppData\LocalLow'
 )
 
-$SOURCE = 'C:\Users\Aryan'
 $CREDLOC = 'C:\Users\Aryan\Documents\PowershellCredentials\graviton.ps1'
 $DRIVE_LTR = 'B'
 
 $destination = "${DRIVE_LTR}:\$ENV:ComputerName"
-$log = "${DRIVE_LTR}:\logs\$(Get-Date -Format FileDate).log"
+$log = "${DRIVE_LTR}:\logs\$ENV:ComputerName\$(Get-Date -Format FileDate).log"
 
 #echo $destination
 #echo $log
-
-
-function unmount-exit {
-	Remove-PSDrive -Name B
-	Exit
-}
 
 . $CREDLOC
 
@@ -35,6 +32,13 @@ $PopUp.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
 $backup_root = '\\' + $BACKUP_SERVER + '\' + $BACKUP_SHARE
 $password = $graviton.Password | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($graviton.Username, $password)
+
+function unmount-exit {
+	Remove-PSDrive -Name B
+	$PopUp.dispose()
+	#Remove-Variable -Name PopUp
+	Exit
+}
 
 #echo $backup_root
  
@@ -62,7 +66,7 @@ if (-not (Test-Path $log)) {
 	$PopUp.Visible = $true
 	$PopUp.ShowBalloonTip(500)
 	
-	unmount-exit
+	#unmount-exit
 }
 
 if (-not (Test-Path $destination)) {
@@ -79,13 +83,25 @@ $PopUp.ShowBalloonTip(500)
 
 $exclude_arg = '/XD '
 foreach ($dirs in $SKIP_DIR) {
-	$exclude_arg += ('"' + $SOURCE + '\' + $dirs + '" ')
+	$exclude_arg += ('"' + $dirs + '" ')
 }
 
 # echo $exclude_arg
 # Exit
 
-Start-Process robocopy -Wait -NoNewWindow -ArgumentList "$SOURCE", "$destination", '/MIR /W:0 /R:2', $exclude_arg, '/XJ', "/LOG+:`"$log`"", '/UNICODE /NS /NC /NP'
+# https://stackoverflow.com/questions/37635820/how-can-i-enumerate-a-hashtable-as-key-value-pairs-filter-a-hashtable-by-a-col/37635938#37635938
+
+foreach ($rdir in $SOURCE_DIR.Keys) {
+	$src  = $SOURCE_DIR[$rdir] # This will get the sorce directory on the local computer
+	$dest = $destination + '\' + $rdir
+	
+	# echo $src
+	# echo $dest
+	# echo $log
+	# echo $exclude_arg
+	
+	Start-Process -FilePath robocopy.exe -Wait -NoNewWindow -ArgumentList "$src", "$dest", '/MIR /W:0 /R:2', $exclude_arg, '/XJ', "/LOG+:`"$log`"", '/UNICODE /NS /NC /NP'
+}
 
 $PopUp.BalloonTipText = 'Backup has finished'
 $PopUp.BalloonTipTitle = 'Backup finished'
