2ea8c5e6fc3b35b9c3dac59a428104f8943cb749
Aryan Gupta <kuantum.freak@gmail.com>
2018-05-07 19:54:54 -0400

    Add code to DNS updater

--- PSScripts/Add-DNSEntry.ps1
+++ PSScripts/Add-DNSEntry.ps1
@@ -1,8 +1,15 @@
 
-$Name = 'test'
-$IP = '192.186.0.7'
-$Type = 'A'
-$Force = $true
+[CmdletBinding()]
+Param (
+	[Parameter (Mandatory = $true, Position = 1)]
+	[string] $Name,
+	
+	[Parameter (Mandatory = $true, Position = 2)]
+	[IPAddress] $IP,
+	
+	[string] $Type = 'A',
+	[switch] $Force
+)
 
 $DNS_SERVER = 'electron'
 $DNS_ROOT   = 'Root'
@@ -23,7 +30,6 @@ $password = $server_cred.Password | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($server_cred.Username, $password)
 
 $root = '\\' + $DNS_SERVER + '.' + $DOMAIN_SUFFIX + '\' + $DNS_ROOT + '\' + $DNS_DIR
-
 New-PSDrive -Name $DRIVE_LTR -PSProvider Filesystem -Root $root -Credential $credential | Out-Null
 
 $splitStr = '; other computers'
@@ -31,19 +37,21 @@ $splitStr = '; other computers'
 # The DNS records are split into 2 parts the pre-text wich includes the current zone
 # and the actual recods for the domains. The first part of the foreach loop goes through
 # skipping all the pre-text. Once it finds the first record (by setting $start high) then
-# the script starts parsing. If 
+# the script starts parsing.
 
 $fwddb = $DRIVE_LTR + ':\' + $DNS_DB
 $start = $false # this bool variable states when we should start parsing the file
 # the file starts of with TTL and Serial data that we want to skip
 $exists = $false
-$lastByteNew = [int]($IP.split('.')[3])
+$lastByteNew = $IP.GetAddressBytes()[3]
 $dbcontent = Get-Content -Path $fwddb
 
 # Prechecks
 # checks that the new hostname doesnt already exists
 # and checks if the new IP adress wont conflict with another entry
 foreach ($line in $dbcontent) {	
+	if ($line -eq '') { continue } # skip empty lines
+	
 	if ($start) { # we have started to parse the actual records
 		$line = $line -replace '\s+', ' ' # we want to delete duplicate spaces
 		$sline = $line.split() # Split by space
@@ -55,7 +63,7 @@ foreach ($line in $dbcontent) {
 			if ($sline[0] -ne $Name) {
 				# If the new IP address will conflict with another record then we want to output
 				# an error
-				Write-Error "IP Conflict found. Host $sline[0] will have same IP as new record"
+				Write-Error "IP Conflict found. Host '$($sline[0])' will have same IP as new record: '$($sline[3])'"
 				Exit
 			} else {
 				Write-Host 'Record already exists, exiting...'
@@ -80,11 +88,12 @@ foreach ($line in $dbcontent) {
 $start = $false
 $newContent = '' # I want to organize the entries by increasing ip number
 # so Im doing going to insert it rather than append it
+$added = $false
 
 foreach ($line in $dbcontent) {
 	$sline = @('') # array of strings
 	
-	if ($start) {
+	if ($start -and ($line -ne '')) { # skip parsing blank lines
 		$linends = $line -replace '\s+', ' ' # we want to delete duplicate spaces
 		$sline = $linends.split() # Split by space
 		
@@ -92,11 +101,12 @@ foreach ($line in $dbcontent) {
 		# split it by period to get the last octet/byte then convert it to a int
 		
 		if ($lastByteExisting -gt $lastByteNew) { #insert new record
-			$newContent += $Name + '     IN      ' + $Type + '       ' + $IP + "`n"
+			$newContent += $Name + '     IN      ' + $Type + '       ' + $IP.toString() + "`n"
 			$start = $false
 			$sline[0] = '' # this is so the if statement later doesn't break
 			# this variable wont be updated anymore, so we need this so it doesnt skip
 			# the rest of the entries
+			$added = $true
 		}
 	}
 	
@@ -111,5 +121,47 @@ foreach ($line in $dbcontent) {
 	}
 }
 
-# add doesn't exist case
-$newContent | Out-File $fwddb
+if (-not $added) { $newContent += $Name + '     IN      ' + $Type + '       ' + $IP.toString() + "`n" }
+
+$newContent | Write-Host
+# $newContent | Out-File $fwddb
+
+# we just added our forward dns info, now reverse dns
+
+$revdb = DRIVE_LTR + ':\' + $DNS_REVDB
+$dbcontent = Get-Content -Path $revdb
+$start = $false
+$newContent = '' # I want to organize the entries by increasing ip number
+# so Im doing going to insert it rather than append it
+$added = $false
+
+foreach ($line in $dbcontent) {
+	$sline = @('') # array of strings
+	
+	if ($start -and ($line -ne '')) { # skip parsing blank lines
+		$linends = $line -replace '\s+', ' ' # we want to delete duplicate spaces
+		$sline = $linends.split() # Split by space
+		
+		$lastByteExisting = [int]($sline[0]) # Get ipadress from line
+		# split it by period to get the last octet/byte then convert it to a int
+		
+		if ($lastByteExisting -gt $lastByteNew) { #insert new record
+			$newContent += $Name + '     IN      ' + $Type + '       ' + $IP.toString() + "`n"
+			$start = $false
+			$sline[0] = '' # this is so the if statement later doesn't break
+			# this variable wont be updated anymore, so we need this so it doesnt skip
+			# the rest of the entries
+			$added = $true
+		}
+	}
+	
+	if ($line -eq $splitStr) { $start = $true } # if its pre-text just skip it
+	
+	if ($exists) { # if it exists we also want to remove the old line
+		if ($sline[0] -ne $Name) {
+			$newContent += $line + "`n" # add other stuff, skip line with old host
+		}
+	} else {
+		$newContent += $line + "`n"
+	}
+}
