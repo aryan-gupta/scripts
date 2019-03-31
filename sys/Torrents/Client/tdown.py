965e52ebcdfe6a46d909afd4759689649f62a61e
Aryan Gupta <kuantum.freak@gmail.com>
2019-03-30 20:02:47 -0400

    Add client initiator script

---
+++ sys/Torrents/Client/tdown.py
@@ -0,0 +1,44 @@
+#!/bin/env python
+
+import paramiko
+import os
+import requests
+from bs4 import BeautifulSoup
+
+def get_server():
+	return "higgs.gempi.re", "stick"
+
+def get_connection():
+	connection = paramiko.SSHClient()
+	connection.load_system_host_keys()
+
+	# Excersize caution when using this command.
+	# connection.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
+
+	host, user = get_server()
+	connection.connect(hostname = host, username = user)
+
+	# Excersize caution when using this command.
+	# connection.save_host_keys(os.path.expanduser("~\.ssh\known_hosts"))
+
+	return connection
+
+def parse_webpage_for_magnet(url):
+	raw_html = requests.get(url)
+	soup = BeautifulSoup(raw_html, 'html.parser')
+	pass
+
+def get_magnet(url):
+	if url.startswith("magnet"):
+		return url
+	elif url.startswith("http"):
+		return None #parse_webpage_for_magnet(url)
+	else:
+		raise ValueError("Invalid url scheme. Must be magnet or webpage")
+
+if __name__ == '__main__':
+	svr = get_connection()
+	command = "tdownstart " + get_magnet()
+	stdin, stdout, stderr = svr.exec_command(command)
+
+	print(stdout.readlines())
