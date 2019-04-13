d5e26873face96ccd33719ff58a8e7570814959a
Aryan Gupta <kuantum.freak@gmail.com>
2019-04-12 23:11:08 -0400

    Update server for more accurate readings

--- sys/Torrents/Server/ipchecktest.py
+++ sys/Torrents/Server/ipchecktest.py
@@ -7,7 +7,7 @@ import protocol as pc
 
 def main():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-		sock.connect(("192.168.0.3", 29630))
+		sock.connect(("home.theguptaempire.net", 29630))
 
 		reply = pc.read_message(sock)
 		print(reply)
