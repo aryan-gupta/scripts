d003de99d2c37f52e379d865215d09698d294c10
Aryan Gupta <kuantum.freak@gmail.com>
2019-04-12 23:07:32 -0400

    Add ip_addr funcs

--- sys/Torrents/Downloader/daemon/src/main.cpp
+++ sys/Torrents/Downloader/daemon/src/main.cpp
@@ -38,3 +38,25 @@ void print_boost_error(const boost::system::error_code& error) {
 	std::cerr << "[E] Value: " << error.value() << std::endl;
 	std::cerr << "[E] Messsage: " << error.message() << std::endl;
 }
+
+
+ip_addr parse_ip(std::string_view sv) {
+	std::string_view::pointer str = sv.data();
+	char* begin;
+	ip_addr ip;
+	ip.b1 = std::strtol(str, &begin, 10);
+	ip.b2 = std::strtol(++begin, &begin, 10);
+	ip.b3 = std::strtol(++begin, &begin, 10);
+	ip.b4 = std::strtol(++begin, &begin, 10);
+	return ip;
+}
+
+
+bool operator== (ip_addr a, ip_addr b) {
+	return a.b1 == b.b1 and a.b2 == b.b2 and a.b3 == b.b3 and a.b4 == b.b4;
+}
+
+
+bool operator!= (ip_addr a, ip_addr b) {
+	return !operator==(a, b);
+}
--- sys/Torrents/Downloader/daemon/src/main.hpp
+++ sys/Torrents/Downloader/daemon/src/main.hpp
@@ -1,6 +1,21 @@
 
+#pragma once
+
 class Connection;
 class Server;
 class TorrentClient;
+
+struct ip_addr {
+	using byte = unsigned char;
+
+	byte b1;
+	byte b2;
+	byte b3;
+	byte b4;
+};
+
+ip_addr parse_ip(std::string_view sv);
+bool operator== (ip_addr a, ip_addr b);
+bool operator!= (ip_addr a, ip_addr b);
 
 void print_boost_error(const boost::system::error_code& error);
