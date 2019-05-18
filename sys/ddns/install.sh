462e14b2d743b022b8ac88d09329e6c9becc723f
Aryan Gupta <kuantum.freak@gmail.com>
2019-05-18 18:26:31 -0400

    Update install script

--- sys/ddns/install.sh
+++ sys/ddns/install.sh
@@ -12,47 +12,45 @@ SCRIPT_FILE="ddns.py"
 SCRIPT_LOC="/bin"
 SCRIPT_NAME="ddnsupdater"
 
-# Install Cred file
+error() {
+	printf '\E[31m'; echo "$@"; printf '\E[0m'
+}
+
+# Check root
+if [[ $EUID -eq 0 ]]; then
+	error "This script should not be run using sudo or as the root user"
+	exit 1
+fi
+
+# Check all the source files exist
 if [[ ! -f "$PWD/$CRED_FILE" ]]; then
-	echo "Could not find cred file: $PWD/$CRED_FILE"
+	error "[E] Could not find cred file: $PWD/$CRED_FILE"
 	exit -1
 fi
 
+if [[ ! -f "$PWD/$SYSTEMD_SERVICE_FILE" ]]; then
+	error "[E] Could not find service file: $PWD/$SYSTEMD_SERVICE_FILE"
+	exit -1
+fi
+
+if [[ ! -f "$PWD/$SCRIPT_FILE" ]]; then
+	error "[E] Could not find script file: $PWD/$SCRIPT_FILE"
+	exit -1
+fi
+
+# Make cred directory
 if [[ ! -d "$CRED_FINAL_LOC" ]]; then
 	mkdir -p "$CRED_FINAL_LOC"
 fi
 
+# Install Cred file
 ln -s "$PWD/$CRED_FILE" "$CRED_FINAL_LOC/$CRED_FILE"
 
-
-
 # Install systemd service file
-if [[ ! -f "$PWD/$SYSTEMD_SERVICE_FILE" ]]; then
-	echo "Could not find cred file: $PWD/$SYSTEMD_SERVICE_FILE"
-	exit -1
-fi
-
-if [[ ! -d "$SYSTEMD_LIB_LOC" ]]; then
-	mkdir -p "$SYSTEMD_LIB_LOC"
-fi
-
-ln -s "$PWD/$SYSTEMD_SERVICE_FILE" "$SYSTEMD_LIB_LOC"
-
-
+cp "$PWD/$SYSTEMD_SERVICE_FILE" "$SYSTEMD_LIB_LOC"
 
 # Install script file
-if [[ ! -f "$PWD/$SCRIPT_FILE" ]]; then
-	echo "Could not find cred file: $PWD/$SCRIPT_FILE"
-	exit -1
-fi
-
-if [[ ! -d "$SCRIPT_LOC" ]]; then
-	mkdir -p "$SCRIPT_LOC"
-fi
-
 ln -s "$PWD/$SCRIPT_FILE" "$SCRIPT_LOC/$SCRIPT_NAME"
-
-
 
 # Start service
 systemctl enable --now "$SYSTEMD_SERVICE_FILE"
