eb8cd02e6a565d2973f819924e6d9408850da660
Aryan Gupta <kuantum.freak@gmail.com>
2019-05-31 14:50:43 -0400

    Update backup script with differential backups

--- osi/AddBackupLoc.py
+++ osi/AddBackupLoc.py
@@ -10,8 +10,6 @@ from sys import argv
 from uriparse import uri_parse
 
 SQL = 'local@higgs.gempi.re/backup'
-BUSVR = 'sftp://higgs.gempi.re'
-BULOC = '/run/sdcard/backup'
 
 
 def backup_table_exists(db, hostname):
@@ -22,29 +20,25 @@ def backup_table_exists(db, hostname):
 
 
 def create_backup_table(db, hostname):
-	# If uname and gname is empty, access is 512, and dest is exclude then it is an exclude directory
-
 	q = f"""CREATE TABLE {hostname} (
 		id INT NOT NULL AUTO_INCREMENT,
+		type CHAR NOT NULL,
 		src TEXT NOT NULL,
-		dest TEXT NOT NULL,
-		uname TINYTEXT,
-		gname TINYTEXT,
-		access SMALLINT,
+		dest TEXT,
 		PRIMARY KEY(id)
 	);"""
 	db.cursor().execute(q)
 
 
-def add_location(db, hostname, src, dest, uname, gname, access):
-	s = "Adding" if access is 512 else "Removing"
+def add_location(db, hostname, src, dest):
+	s, dest, butype = ("Removing", 'NULL', '-') if dest is None else ("Adding", dest, '+')
+
 	print(f"[I] {s} `{src}' to list of backup locations")
-	q = f"""INSERT INTO {hostname} (src, dest, uname, gname, access) VALUES (
+
+	q = f"""INSERT INTO {hostname} (type, src, dest) VALUES (
+		"{butype}"
 		"{src}",
-		"{dest}",
-		"{uname}",
-		"{gname}",
-		{access}
+		"{dest}"
 	);"""
 
 	db.cursor().execute(q)
@@ -62,15 +56,19 @@ def get_database(uri):
 	except mysql.connector.errors.ProgrammingError:
 		print("[E] Error on connecting to database")
 
-
-def get_file_details(filename):
-	s = stat(filename)
-	return (
-		getpwuid(s.st_uid).pw_name,
-		getgrgid(s.st_gid).gr_name,
-		int(s.st_mode & 0o777)
-	)
-
+# Backups work like this: The SQL server has a db like this
+# id (INT)    type (CHAR)       src (TEXT)         dest(TEXT)
+# 0             +/-            /home/test/sada        /root/home/test/big/sada
+#
+# The id is autoincremented, the type is whether the path is an include or exclude
+# The src and dest are self explanatory. for excludes, dest is `excluded'
+# The dest path always references its path to /root. The /root/ directory stores
+# the default backup of the rootfs. If any file must be backed up, it can be stores in
+# another folder not in /root/. The backup server appends the dest to the backup
+# location path, the client name, and the date. So the final path to the backup
+# location for the above directory on the server would be
+# /run/sdcard/backups/boson/20190531-2150/root/home/test/big/sada
+# {backup_location}/{hostname}/{YYYYMMDD-HHMM}/{backup_type}/{path}
 
 def loc_exists(db, hostname, src, dest):
 	q = f"""SELECT src, dest FROM {hostname} WHERE src='{src}' AND dest='{dest}'"""
@@ -81,7 +79,7 @@ def loc_exists(db, hostname, src, dest):
 	return response
 
 
-def add_backup_location(exclude, src, dest, hostname):
+def add_backup_location(hostname, src, dest):
 	db = get_database(SQL)
 
 	if not backup_table_exists(db, hostname):
@@ -89,39 +87,34 @@ def add_backup_location(exclude, src, dest, hostname):
 		create_backup_table(db, hostname)
 
 	if not loc_exists(db, hostname, src, dest):
-		if exclude:
-			user, group, access = '', '', 512
-			add_location(db, hostname, src, dest, user, group, access)
-		else:
-			user, group, access = get_file_details(src)
-			add_location(db, hostname, src, dest, user, group, access)
+		add_location(db, hostname, src, dest)
 	else:
 		print("[E] Location already exits")
 
 
 def main():
 	if len(argv) == 4:
-		sub = argv[1] == '-'
-		if sub: raise ValueError("[E] Removeing a backup directory cannot have a dest param")
+		exclude = argv[1] == '-'
+		if exclude: raise ValueError("[E] Excluding a backup directory cannot have a dest param")
 		src  = argv[2]
 		dest = argv[3]
 	elif len(argv) == 3:
 		if argv[1] == '-' or argv[1] == '+':
-			sub = argv[1] == '-'
+			exclude = argv[1] == '-'
 			src  = argv[2]
 			dest = None
 		elif argv[2] == '-' or argv[2] == '+':
-			sub = argv[2] == '-'
+			exclude = argv[2] == '-'
 			src  = argv[1]
 			dest = None
 		else:
-			sub = False
+			exclude = False
 			src  = argv[1]
 			dest = argv[2]
 	elif len(argv) == 2:
 		if argv[1] == '-' or argv[1] == '+':
 			raise ValueError(f"[E] Usage: python {__file__} [+|-] <src> [<dest>]")
-		sub = False
+		exclude = False
 		dest = None
 		src  = argv[1]
 	else:
@@ -132,18 +125,16 @@ def main():
 
 	# @todo make sure that we haven't accidentally added +/- to the start of src
 
-	if sub:
-		dest = 'exclude'
-	elif dest is None:
-		dest = BUSVR + path.join(BULOC, f"{hostname}{src}")
+	if exclude:
+		dest = None
+	elif dest is None: # including needs a dest param
+		dest = '/root' + src # src is an abs path so it will have the '/'
 	else:
-		base = dest.replace('\\', '/')
-		if base[0] == '/':
-			dest = BUSVR + base
-		else:
-			dest = BUSVR + path.join(BULOC, base)
+		dest = dest.replace('\\', '/')
+		if dest[0] != '/':
+			raise ValueError(f'[E] dest parameter must be referenced by a root (i.e. start with a `/\' or `\\\')')
 
-	add_backup_location(sub, src, dest, hostname)
+	add_backup_location(hostname, src, dest)
 
 
 if __name__ == '__main__':
--- osi/BackupSystem.py
+++ osi/BackupSystem.py
@@ -2,10 +2,43 @@
 
 import mysql.connector
 from socket import gethostname
-from subprocess import call
 from uriparse import uri_parse
 import os
 import subprocess
+from datetime import datetime
+import warnings
+warnings.filterwarnings(action='ignore',module='.*paramiko.*')
+import paramiko
+
+SQL = "mysql://local@higgs.gempi.re/backup"
+BU_SVR = 'ssh://stick@higgs.gempi.re'
+BU_ROOT = '/run/sdcard/backups'
+
+def get_ssh_connection(svr):
+	client = paramiko.SSHClient()
+	client.load_system_host_keys()
+	ssh_config = paramiko.SSHConfig()
+
+	user_config_file = os.path.expanduser("~/.ssh/config")
+	if os.path.exists(user_config_file):
+		with open(user_config_file) as file:
+			ssh_config.parse(file)
+
+	server = uri_parse(svr)
+	if server.scheme != 'ssh': raise ValueError('[E] Not a link to a ssh server')
+
+	config = ssh_config.lookup(server)
+
+	client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
+
+	client.connect(
+		hostname=config['hostname'] if 'user' in config.keys() else 'higgs.gempi.re',
+		username=config['user'] if 'user' in config.keys() else 'stick',
+		port=config['port'] if 'port' in config.keys() else 22,
+		password='***REMOVED***'
+	)
+
+	return client
 
 def get_database(svr):
 	uri = uri_parse(svr)
@@ -28,44 +61,60 @@ def get_backup_locations(db, hostname):
 	cur.execute(q)
 	result = cur.fetchall()
 	ret_val = []
-	for lst in result: # convert response into a list of touples
+	for lst in result: # convert response into a list of tuples
 		ret_val.append((lst[0], lst[1]))
 	return ret_val
 
-def mount_backup_loc(svr, loc):
-	uri = uri_parse(svr) # parse server uri
-	mountpt = os.path.join(loc, uri.host.split('.')[0])
-	os.makedirs(mountpt, exist_ok=True)
-	cmd = f"mount.cifs -v -o user={uri.user},vers=3.0 //{uri.host}{uri.path} {mountpt}"
-	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-	if error:
-		print(error + ": ")
-		print(output)
-	return mountpt
+def run_copy(client, src, dest):
+	cmd = f"cp -al {src} {dest}"
+	print(cmd)
+	return client.exec_command(cmd)
 
-def umount_backup_loc(mountpt):
-	cmd = f"umount {mountpt}"
-	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-	if error:
-		print(error + ": ")
-		print(output)
+def run_rsync(src, dest, excludes):
+	cmd = ['rsync', '-vaiS', '--delay-updates', '--delete', 'src', 'dest', f'--log-file=/var/test.txt']
+	for exc in excludes:
+		cmd.append(f'--exclude={exc}')
+	cmd[4] = src + '/'
+	cmd[5] = dest
+	print(' '.join(cmd))
+	return
+	subprocess.run(cmd, capture_output=True)
 
-def backup_files(loc):
-	cmd = f"rsync -avuzb"
-	pass
-	
+def get_last_backup(client, root):
+	cmd = f"ls {root}"
+	print(cmd)
+	return
+	_, stdout, _ = client.exec_command(cmd)
+	folders = stdout.split(' ')
+	last = folders[len(folders) - 1]
+	return datetime.strptime(last, '%Y%m%d-%H%M') if len(folders) > 0 else None
 
-def check_sudo():
-	if os.getuid() is not 0:
-		raise PermissionError("[E] This scripts must be run with admin credentials")
+def backup_files(locs): # locs are a 3-tuple of src, dest, and excludes
+	root = os.path.join(BU_ROOT, gethostname())
+	client = get_ssh_connection(BU_SVR)
+	last_bu = get_last_backup(client, root)
+	date = datetime.strftime(datetime.now(), '%Y%m%d-%H%M')
+
+	if last_bu is None:
+		for loc in locs:
+			run_rsync(loc[0], os.path.join(root, date, loc[1][1:]), [])
+	else:
+		last = os.path.join(root, last_bu)
+		new = os.path.join(root, date)
+		run_copy(client, last, new)
+		for loc in locs:
+			run_rsync(loc[0], os.path.join(root, date, loc[1][1:]), [])
+
+def main():
+	db = get_database(SQL)
+	loc = get_backup_locations(db, gethostname())
+	backup_files(loc)
 
 if __name__ == '__main__':
-	check_sudo()
-	# valid from here: https://dev.mysql.com/doc/refman/8.0/en/connecting-using-uri-or-key-value-pairs.html
-	db = get_database("mysql://local@higgs.gempi.re/backup")
-	loc = get_backup_locations(db, gethostname())
-	mountpt = mount_backup_loc("stick@higgs.gempi.re/storage", "/media")
-	backup_files(loc)
-	umount_backup_loc(mountpt)
+	main()
+
+## Create links
+# cp -al dest/initial dest/first
+
+## Sync
+# rsync -vaiS --delay-updates --delete src/ dest/first
