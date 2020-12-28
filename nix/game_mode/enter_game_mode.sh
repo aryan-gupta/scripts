462a0e5486c27b570031e496571cdf3083ddcacc
Aryan Gupta <kuantum.freak@gmail.com>
2020-12-27 19:10:12 -0500

    Add version 2 of enter_game_mode.sh

--- nix/game_mode/enter_game_mode.sh
+++ nix/game_mode/enter_game_mode.sh
@@ -3,6 +3,10 @@
 sudo echo "Starting"
 sudo modprobe i2c-dev
 sudo virsh start win10-gaming-vm
-sleep 30
-sudo /var/lib/libvirt/images/win10_attach.sh &&
+
+if [ $? -ne 0 ]; then
+	sleep 30
+fi
+
+sudo ./win10_attach.sh &&
 sudo ddccontrol -r 0x60 -w 18 dev:/dev/i2c-6
