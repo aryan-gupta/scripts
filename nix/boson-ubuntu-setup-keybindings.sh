2711c4a9961b895f6d1c696c4af7922866ad44d8
Aryan Gupta <me@theguptaempire.net>
2022-10-12 15:06:09 -0400

    Update boson-ubuntu keybind config to use alacritty

--- nix/boson-ubuntu-setup-keybindings.sh
+++ nix/boson-ubuntu-setup-keybindings.sh
@@ -16,7 +16,7 @@ function set_custom_keybinds() {
 set_custom_keybinds
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'terminal_launch'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Alt>t'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'/bin/gnome-terminal'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'/usr/bin/alacritty'"
 
 set_custom_keybinds
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "'web_browser_launch'"
